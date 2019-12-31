d813579339a4f629c030d0e2501d8b85910fd4d9
Facundo Palombo <facundo.palombo@gmail.com>
2019-12-31 10:54:22 -0300

    Creating singleton instance and methods for consuming mocked api

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React, { useEffect, useState } from 'react'
 import './styles.css'
 import './App.scss'
 import Axios from 'axios'
+import { Products } from './utils/mocks/services/api'
+
 /**
  * @template App
  * Application template
  */
 export const App = () => {
   const [data, setData] = useState({})
-  useEffect(async () => {
-    const result = await Axios.get('https://jsonplaceholder.typicode.com/users')
-    setData(result.data)
+  useEffect(() => {
+    const products = new Products()
+    products
+      .deleteOne(item => item.email === 'emenhenitt0@smugmug.com')
+      .then(data => setData(data))
+      .catch(err => console.error(err))
   }, [])
-
+  console.log(data)
   return <div>asd</div>
 }
--- src/utils/mocks/services/api.js
+++ src/utils/mocks/services/api.js
@@ -1,18 +1,41 @@
-import * as products from '../products.json.js'
-import * as users from '../users.json.js'
+import * as products from '../products.json'
+import * as users from '../users.json'
 
 class Api {
   constructor(items) {
-    this.items = items
+    this.items = items || []
   }
-  async getAll({ filter }) {
-    const data = filter ? { ...this.items[filter] } : { ...this.items }
-    delete data.id, data.uid
+  //Singleton Factory instance
+  connect() {
+    if (!Api.collection) {
+      Api.collection = new Promise((resolve, reject) => {
+        this.items === [] ? reject('Empty collection, please use mocked data in order to use this false api') : resolve(this.items.default)
+      })
+    }
+    return Api.collection
+  }
+
+  async getAll(filter = null) {
+    const data = await this.connect().then(data => (filter ? data.filter(filter) : data))
     return Promise.resolve(data)
   }
-  async getOne({ filter }) {
-    const data = { ...this.items[filter] }
-    delete data.id, data.uid
+  async getOne(filter) {
+    const data = await this.connect().then(data => data.filter(filter))
+    return Promise.resolve(data)
+  }
+  async deleteOne(filter) {
+    const data = await this.connect().then(data => {
+      data.filter(filter).splice()
+      data.push({ mocked: 'data' })
+      return data
+    })
+    return Promise.resolve(data)
+  }
+  async createOne(item) {
+    const data = await this.connect().then(data => {
+      data.push(item)
+      return data
+    })
     return Promise.resolve(data)
   }
 }
